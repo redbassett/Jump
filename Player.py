2606c92e551a12ac07eadfcdc7d6a4d4cde6696c
Harry Grillo <redbassett@gmail.com>
2012-04-08 18:47:15 -0400

    Figuring out keypress functions.

--- Player.py
+++ Player.py
@@ -4,12 +4,17 @@ import pygame
 """
 A player is a character in the game.  All players are human controlled.
 """
+
 class Player(object):
-    def __init__(self, color = (255,255,255)):
-        self.x, self.y  = (0,0)
-        self.w, self.h = (20,20)
+    def __init__(self, (x,y), color = (255,255,255), (w,h) = (20,20)):
+        self.x, self.y  = (x,y)
+        self.w, self.h = (w,h)
         self.color = color
     
     def draw(self, screen):
         rect = pygame.Rect((self.x, self.y), (self.w, self.h))
         pygame.draw.rect(screen, self.color, rect)
+    
+    def move(self, (x, y)):
+        self.x += x
+        self.y += y
