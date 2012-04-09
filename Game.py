94d7cdd41ce7489650dd92c783f101e76e0e736f
Harry Grillo <redbassett@gmail.com>
2012-04-09 10:26:00 -0400

    Cleaned up Game code, started to implement camera.

--- Game.py
+++ Game.py
@@ -20,14 +20,11 @@ class Game(object):
         self.screen = screen
         self.level = Level()
         self.spawn()
-
-        #self.cam = Camera(self.player, self.level.bounds, self.game_area.get_size())
         
-
+        self.game_area = screen.subsurface((0, 40, screen.get_width(), screen.get_height() - 40))
+        self.cam = Camera(self.player, self.level.bounds, self.game_area.get_size())
+        
         # self.hud = screen.subsurface((0, 0, screen.get_width(), 40))
-        # self.game_area = screen.subsurface((0, 40, screen.get_width(), screen.get_height() - 40))
-        #How do we use this? It needs to display deaths
-    
     
     def spawn(self):
         self.player = Player((self.level.spawnPoint), self.level, self)
@@ -51,10 +48,6 @@ class Game(object):
 
             #update
             self.player.update(dt)
-           
-            #for collide in spritecollide(self.player, self.lethal, True):
-             #   self.deaths += 1
-            #This needs to also be set to restart the player in its original position.
 
             #draw
             self.screen.fill((0,0,0))
